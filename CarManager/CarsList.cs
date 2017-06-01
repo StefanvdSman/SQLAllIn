f394a61884ed6cd4bd9698c231cd167acfa8067c
Stefan van der Sman <sj.vandersman@gmail.com>
2017-06-01 09:32:10 +0200

    Add project files.

---
+++ CarManager/CarsList.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CarManager.Models;
+using CarManager.DatabaseControllers;
+
+namespace CarManager
+{
+    public partial class CarsList : Form
+    {
+        private CarDBController _carDB; 
+        public CarsList()
+        {
+            _carDB = new CarDBController();
+            InitializeComponent();
+            InitializeList();
+           
+        }
+        List<Car> cars;
+        private void InitializeList()
+        {
+           
+            lstCars.Items.Clear();
+            cars = _carDB.GetAllCars();
+            /*{
+                new Car() {ID = 1, Brand = "BMW", Model = "X5", SUV = true },
+                new Car() {ID = 2, Brand = "VW", Model = "Golf", SUV = false },
+                new Car() {ID = 3, Brand = "Audi", Model = "Q7", SUV = true },
+                new Car() {ID = 4, Brand = "Mercedes", Model = "GLS", SUV = true },
+                new Car() {ID = 1, Brand = "Mini", Model = "Cooper S", SUV = false }
+            };*/
+            
+            foreach (Car car in cars)
+            {
+                lstCars.Items.Add(car);
+            }
+        }
+
+        public void SaveNewCar(Car car)
+        {
+            car.ID = lstCars.Items.Cast<Car>().Max(x => x.ID) + 1;
+            lstCars.Items.Add(car);
+            _carDB.InsertCar(car);
+        }
+
+        public void SaveEditCar(Car car)
+        {
+            int index = -1;
+            for (int i = 0; i < lstCars.Items.Count; i++)
+            {
+                Car c = lstCars.Items[i] as Car;
+                if(c.ID == car.ID)
+                {
+                    index = i;
+                }
+            }
+            lstCars.Items[index] = car;
+            _carDB.EditCar(car);
+        }
+
+        private void RemoveCar(Car car)
+        {
+            lstCars.Items.Remove(car);
+            _carDB.DeleteCar(car);
+        }
+
+        private void btnAddCar_Click(object sender, EventArgs e)
+        {
+            new CarEdit(this).Show();
+            this.Hide();
+        }
+
+        private void btnRemoveCar_Click(object sender, EventArgs e)
+        {
+            if(lstCars.SelectedItem != null)
+            {
+                RemoveCar((Car)lstCars.SelectedItem);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (lstCars.SelectedItem != null)
+            {
+                new CarEdit(this, (Car)lstCars.SelectedItem).Show();
+                this.Hide();
+            }
+        }
+
+        private void CarsList_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ CarManager/DatabaseControllers/CarDBController.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CarManager.Models;
+using MySql.Data.MySqlClient;
+
+namespace CarManager.DatabaseControllers
+{
+    public class CarDBController : DatabaseController
+    {
+        public List<Car> GetAllCars()
+        {
+            List<Car> cars = new List<Car>();
+            try
+            {
+                conn.Open();
+                string query = "SELECT * FROM cars;";
+                MySqlCommand cmd = new MySqlCommand(query, conn);
+                MySqlDataReader dataReader = cmd.ExecuteReader();
+                
+                while(dataReader.Read())
+                {
+                    Car car = new Car()
+                    {
+                        ID = dataReader.GetInt32("id"),
+                        Brand = dataReader.GetString("brand"),
+                        Model = dataReader.GetString("model"),
+                        SUV = dataReader.GetBoolean("suv")
+                    };
+
+                    cars.Add(car);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.Write("Er ging iets fout bij het ophalen van een car: " + e);
+            }
+            finally
+            {
+                conn.Close();
+            }
+
+            return cars;
+        }
+
+        public void InsertCar(Car car)
+        {
+            MySqlTransaction trans = null;
+            try
+            {
+                conn.Open();
+                trans = conn.BeginTransaction();
+                string query = @"INSERT INTO cars (brand, model, suv) VALUES (@brand, @model, @suv);";
+
+                MySqlCommand cmd = new MySqlCommand(query, conn);
+                MySqlParameter brandParam = new MySqlParameter("@brand", MySqlDbType.VarChar);
+                MySqlParameter modelParam = new MySqlParameter("@model", MySqlDbType.VarChar);
+                MySqlParameter suvParam = new MySqlParameter("@suv", MySqlDbType.Bit);
+
+                brandParam.Value = car.Brand;
+                modelParam.Value = car.Model;
+                suvParam.Value = car.SUV;
+
+                cmd.Parameters.Add(brandParam);
+                cmd.Parameters.Add(modelParam);
+                cmd.Parameters.Add(suvParam);
+
+                cmd.Prepare();
+                cmd.ExecuteNonQuery();
+
+                trans.Commit();
+            }
+            catch(Exception e)
+            {
+                trans.Rollback();
+                Console.Write("Er ging iets fout bij het inserten van een car: " + e);
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+        public void DeleteCar(Car car)
+        {
+            MySqlTransaction trans = null;
+            try
+            {
+                conn.Open();
+                trans = conn.BeginTransaction();
+                string query = @"DELETE FROM cars WHERE id=(@id);";
+
+                MySqlCommand cmd = new MySqlCommand(query, conn);
+                MySqlParameter idParam = new MySqlParameter("@id", MySqlDbType.Int16);
+                              
+                idParam.Value = car.ID;
+
+                cmd.Parameters.Add(idParam);
+               
+                cmd.Prepare();
+                cmd.ExecuteNonQuery();
+
+                trans.Commit();
+            }
+            catch (Exception e)
+            {
+                trans.Rollback();
+                Console.Write("Er ging iets fout bij het deleten van een car: " + e);
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+        public void EditCar(Car car)
+        {
+            MySqlTransaction trans = null;
+            try
+            {
+                conn.Open();
+                trans = conn.BeginTransaction();
+                string query = @"UPDATE cars
+    SET brand = (@brand),
+        model = (@model),
+        suv   = (@suv) WHERE id =(@id);
+    ";
+
+                MySqlCommand cmd = new MySqlCommand(query, conn);
+                MySqlParameter brandParam = new MySqlParameter("@brand", MySqlDbType.VarChar);
+                MySqlParameter modelParam = new MySqlParameter("@model", MySqlDbType.VarChar);
+                MySqlParameter suvParam = new MySqlParameter("@suv", MySqlDbType.Bit);
+                MySqlParameter idParam = new MySqlParameter("@id", MySqlDbType.Int16);
+
+                idParam.Value = car.ID;
+                brandParam.Value = car.Brand;
+                modelParam.Value = car.Model;
+                suvParam.Value = car.SUV;
+
+                cmd.Parameters.Add(brandParam);
+                cmd.Parameters.Add(modelParam);
+                cmd.Parameters.Add(suvParam);
+                cmd.Parameters.Add(idParam);
+
+                cmd.Prepare();
+                cmd.ExecuteNonQuery();
+
+                trans.Commit();
+            }
+            catch (Exception e)
+            {
+                trans.Rollback();
+                Console.Write("Er ging iets fout bij het inserten van een car: " + e);
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+    }
+}
